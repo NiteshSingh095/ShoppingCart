f7842ad0c449ee736b986bc4f4c44d12aeb6b8ca
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2023-05-28 00:00:38 +0530

    ui changes done

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -8,20 +8,38 @@ class productListScreen extends StatefulWidget {
   State<productListScreen> createState() => _productListScreenState();
 }
 
-class _productListScreenState extends State<productListScreen>
-{
-  List<String> productName = ['Mango' , 'Orange' , 'Grapes' , 'Banana' , 'Chery' , 'Peach','Mixed Fruit Basket',] ;
-  List<String> productUnit = ['KG' , 'Dozen' , 'KG' , 'Dozen' , 'KG' , 'KG','KG',] ;
-  List<int> productPrice = [10, 20 , 30 , 40 , 50, 60 , 70 ] ;
+class _productListScreenState extends State<productListScreen> {
+  List<String> productName = [
+    'Mango',
+    'Orange',
+    'Grapes',
+    'Banana',
+    'Chery',
+    'Peach',
+    'Mixed Fruit Basket',
+  ];
+
+  List<String> productUnit = [
+    'KG',
+    'Dozen',
+    'KG',
+    'Dozen',
+    'KG',
+    'KG',
+    'KG',
+  ];
+
+  List<int> productPrice = [10, 20, 30, 40, 50, 60, 70];
+
   List<String> productImage = [
-    'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg' ,
-    'https://image.shutterstock.com/image-photo/orange-fruit-slices-leaves-isolated-600w-1386912362.jpg' ,
-    'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg' ,
-    'https://media.istockphoto.com/photos/banana-picture-id1184345169?s=612x612' ,
-    'https://media.istockphoto.com/photos/cherry-trio-with-stem-and-leaf-picture-id157428769?s=612x612' ,
-    'https://media.istockphoto.com/photos/single-whole-peach-fruit-with-leaf-and-slice-isolated-on-white-picture-id1151868959?s=612x612' ,
-    'https://media.istockphoto.com/photos/fruit-background-picture-id529664572?s=612x612' ,
-  ] ;
+    'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg',
+    'https://image.shutterstock.com/image-photo/orange-fruit-slices-leaves-isolated-600w-1386912362.jpg',
+    'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg',
+    'https://media.istockphoto.com/photos/banana-picture-id1184345169?s=612x612',
+    'https://media.istockphoto.com/photos/cherry-trio-with-stem-and-leaf-picture-id157428769?s=612x612',
+    'https://media.istockphoto.com/photos/single-whole-peach-fruit-with-leaf-and-slice-isolated-on-white-picture-id1151868959?s=612x612',
+    'https://media.istockphoto.com/photos/fruit-background-picture-id529664572?s=612x612',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,24 +47,29 @@ class _productListScreenState extends State<productListScreen>
       appBar: AppBar(
         title: Text("Product List"),
         centerTitle: true,
-        actions: const[
+        actions: const [
           Center(
             child: Badge(
-              badgeContent: Text("0", style: TextStyle(color: Colors.white),),
-              badgeAnimation: BadgeAnimation.slide(animationDuration: Duration(milliseconds: 300)),
+              badgeContent: Text(
+                "0",
+                style: TextStyle(color: Colors.white),
+              ),
+              badgeAnimation: BadgeAnimation.slide(
+                  animationDuration: Duration(milliseconds: 300)),
               child: Icon(Icons.shopping_bag_outlined),
             ),
           ),
-          SizedBox(width: 20,)
+          SizedBox(
+            width: 20,
+          )
         ],
       ),
       body: Column(
         children: [
           Expanded(
               child: ListView.builder(
-                itemCount: productName.length,
-                  itemBuilder: (context, index)
-                  {
+                  itemCount: productName.length,
+                  itemBuilder: (context, index) {
                     return Card(
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
@@ -62,42 +85,56 @@ class _productListScreenState extends State<productListScreen>
                                 Image(
                                   height: 100,
                                   width: 100,
-                                  image: NetworkImage(productImage[index].toString()
-                                  ),
+                                  image: NetworkImage(
+                                      productImage[index].toString()),
                                 ),
-                                const SizedBox(width: 10,),
+                                const SizedBox(
+                                  width: 10,
+                                ),
                                 Expanded(
                                   child: Column(
                                     mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     children: [
-                                      Text(productName[index].toString(),
-                                        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                                      Text(
+                                        productName[index].toString(),
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500),
                                       ),
-
-                                      const SizedBox(height: 5,),
-
-                                      Text(r"$" + productPrice[index].toString() + " / " + productUnit[index].toString(),
-                                        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                                      const SizedBox(
+                                        height: 5,
                                       ),
-
-                                      const SizedBox(height: 5,),
-
+                                      Text(
+                                        r"$" +
+                                            productPrice[index].toString() +
+                                            " / " +
+                                            productUnit[index].toString(),
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
                                       Align(
-                                      alignment: Alignment.bottomRight,
-                                      child : Container(
-                                        height: 35,
-                                        width: 100,
-                                        decoration: BoxDecoration(
-                                          color: Colors.green,
-                                          borderRadius: BorderRadius.circular(5)
-                                        ),
-                                        child: const Center(
-                                            child:Text("Add To Cart", 
-                                              style: TextStyle(fontWeight: FontWeight.w500),
+                                        alignment: Alignment.bottomRight,
+                                        child: Container(
+                                          height: 35,
+                                          width: 100,
+                                          decoration: BoxDecoration(
+                                              color: Colors.green,
+                                              borderRadius:
+                                                  BorderRadius.circular(5)),
+                                          child: const Center(
+                                            child: Text(
+                                              "Add To Cart",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.w500),
                                             ),
+                                          ),
                                         ),
-                                      ),
                                       ),
                                     ],
                                   ),
@@ -108,9 +145,7 @@ class _productListScreenState extends State<productListScreen>
                         ),
                       ),
                     );
-                  }
-              )
-          )
+                  }))
         ],
       ),
     );
