42dd915a69a3cbd5b2c1f9b885ac592bb4fa0fbf
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2023-05-31 13:52:29 +0530

    adding provider for state management

---
+++ lib/cart_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CartProvider with ChangeNotifier
+{
+  int _counter = 0;
+  int get counter => _counter;
+
+  double _totalPrice = 0.0;
+  double get totalPrice => _totalPrice;
+
+  void _setPrefItems() async
+  {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('cart_item', _counter);
+    prefs.setDouble('total_Price', _totalPrice);
+    notifyListeners();
+  }
+
+  void _getPrefItems() async
+  {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _counter = prefs.getInt('cart_item') ?? 0;
+    _totalPrice = prefs.getDouble('total_price') ?? 0.0;
+    notifyListeners();
+  }
+
+}
